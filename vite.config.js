759a5a646355b121aca046a9e20405630671b8d8
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-09-03 20:54:43 -0600

    chore: Refactor file paths in jsconfig and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      assets: "/src/assets",
-      config: "/src/config",
-      styles: "/src/components/styles",
-      ui: "/src/components/ui",
-      utils: "/src/components/utils",
-      views: "/src/components/views",
-    },
-  },
 });
