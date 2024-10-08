fc003a34ccf657eafb8c0f7ecd6d8345a71efa67
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-10-08 12:09:24 -0600

    Refactor App component and add new functionality
    - Refactor App component to improve code structure and readability
    - Add new functionality to toggle theme between light and dark mode
    - Update CRInput component to use a more descriptive placeholder
    - Update CRButton component to display an icon and handle reset functionality
    - Remove unused code and comments

--- src/components/views/App.jsx
+++ src/components/views/App.jsx
@@ -6,24 +6,26 @@ import CRDate from "../UI/CRDate";
 import CRButton from "../UI/CRButton";
 
 const App = () => {
-  const [prueba, setPrueba] = useState("");
-  const [resetear, setResetear] = useState(false);
   const { theme, setTheme } = useTheme();
 
+  // ##### ESTADOS #####
+  const [prueba, setPrueba] = useState("");
+  const [resetear, setResetear] = useState(false);
+
+  // ##### RENDER #####
   return (
     <div className="w-full h-screen bg-white dark:bg-neutral-800 flex justify-center items-center">
       <div className="w-1/2 justify-center m-auto p-5">
-        {/* // ADD BUTTON rigth top absulute to change the theme */}
         <button className="absolute right-0 top-0" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
           {theme === "light" ? "🌙" : "☀️"}
         </button>
 
-        <CRInput title="Nombre" placeholder="sotz" setValue={setPrueba} reset={resetear} maxLength={50} />
+        {/* /////////////////////////////////////// COMPONENTES /////////////////////////////////////// */}
+        <CRInput title="Nombre" placeholder="Escriba su nombre" setValue={setPrueba} reset={resetear} />
         <CRDate title="Fecha" setValue={setPrueba} reset={resetear} />
-        <CRButton title="Resetear" onClick={() => setResetear(!resetear)} />
+        <CRButton title="Resetear" icon="close" onClick={() => setResetear(!resetear)} />
 
-        {/* ############# PARA LAS PRUEBAS ############# */}
-
+        {/* ##### PARA LAS PRUEBAS ##### */}
         {prueba && (
           <p className="text-center text-xl mt-2">
             {prueba}, {resetear ? "true" : "false"}
